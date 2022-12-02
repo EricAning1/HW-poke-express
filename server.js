6ed403883cdade04139634cbe0081fcaf709c2ee
EricAning1 <ericaning@hotmail.com>
2022-12-02 06:52:12 -0600

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const pokemon = require('./models/pokemon.js');
+const expressReactViews = require('express-react-views');
 
 app.get('/', function (req, res) {
   res.send('Welcome to the Pokemon App!');
 });
 
+app.set('view engine', 'jsx');
+app.engine('jsx', expressReactViews.createEngine());
+
 app.get('/pokemon', function (req, res) {
-  res.send('pokemon');
+  res.render('Index');
 });
 
 app.listen(port, function () {
